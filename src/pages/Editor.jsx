0d8dc8d859b1706365cc75c0028e30c941bff8a6
vatsalkr123 <rajsinghskrk6@gmail.com>
2024-06-24 23:23:59 +0530

    resizeable modes are added

--- src/pages/Editor.jsx
+++ src/pages/Editor.jsx
@@ -17,7 +17,10 @@ const EditorPage = () => {
   const [inputPrompt, setInputPrompt] = useState("");
   const [outputData, setOutputData] = useState([]);
   const [isWaiting, setIsWaiting] = useState(false);
-  const [inputData, setInputData] = useState('')
+  const [inputData, setInputData] = useState("");
+  const [sideBarWidth, setSideBarWidth] = useState(200)
+  const [editorHeight, setEditorHeight] = useState(403)
+  const [terminalHeight, setTerminalHeight] = useState(200)
   const [editorContent, setEditorContent] = useState({
     lang: language,
     code: "",
@@ -25,12 +28,11 @@ const EditorPage = () => {
     type: "",
   });
   const handleInput = (e) => {
-    setInputData(e.target.value)
-    if(e.key==="Enter") {
+    setInputData(e.target.value);
+    if (e.key === "Enter") {
       socketio.emit("input", e.target.value);
-      e.target.value=""
+      e.target.value = "";
     }
-   
   };
   const terminalComponents = {
     Problem: <Problem />,
@@ -47,8 +49,8 @@ const EditorPage = () => {
 
   const handleSubmit = () => {
     setOutputData("");
-    const input = document.getElementById('input-box');
-    input.value=""
+    const input = document.getElementById("input-box");
+    input.value = "";
     const updatedContent = {
       ...editorContent,
       lang: language,
@@ -62,19 +64,17 @@ const EditorPage = () => {
   useEffect(() => {
     socketio.on("code", (data) => {
       const cleanedCode = data.code;
-      if (data.type === "info" ) {
+      if (data.type === "info") {
         setIsWaiting(true);
-        const input = document.getElementById('input-box');
-        input.focus()
+        const input = document.getElementById("input-box");
+        input.focus();
         setInputPrompt(cleanedCode);
-        
       } else {
-        setIsWaiting(false)
+        setIsWaiting(false);
         setInputPrompt("");
       }
-      console.log(cleanedCode)
+      console.log(cleanedCode);
       setOutputData((prevOutputData) => [...prevOutputData, cleanedCode]);
-     
     });
     // socketio.on("inputPrompt",(data)=>{
     //   console.log("Entered here")
@@ -83,10 +83,9 @@ const EditorPage = () => {
     // Cleanup listener on unmount
     return () => {
       socketio.off("code");
-      socketio.off("inputPrompt")
+      socketio.off("inputPrompt");
     };
   }, []);
-
 
   // const handleValidation = (markers)=> {
   //   markers.forEach(marker=> console.log('onValidate', marker.message))
@@ -105,9 +104,59 @@ const EditorPage = () => {
     setLanguage(e.currentTarget.value);
   };
 
+  useEffect(()=> {
+    const sideBar = document.getElementById('sidebar');
+    const resizeLine = document.getElementById('sidebar-resize-line');
+    window.addEventListener('mousemove', function(e) {
+      
+    })
+
+    resizeLine.addEventListener('mousedown', function(e){
+      e.preventDefault();
+      window.addEventListener('mousemove', resize);
+      window.addEventListener('mouseup', stopResize);
+    })
+
+    function resize(e) {
+      if(e.pageX>=200 && e.pageX<=window.innerWidth-400) {
+        sideBar.style.width = e.pageX + 'px';
+        setEditorWidth(window.innerWidth - e.pageX)
+        setSideBarWidth(e.pageX)
+      }
+    }
+
+    function stopResize() {
+      window.removeEventListener('mousemove', resize);
+    }
+  }, [])
+
+  useEffect(() => {
+    const resizeLine = document.getElementById("resize-line");
+    const terminalContainer = document.getElementById("terminal-container");
+
+    resizeLine.addEventListener("mousedown", function (e) {
+      e.preventDefault();
+
+      window.addEventListener("mousemove", resize);
+      window.addEventListener("mouseup",stopResize);
+    });
+    function resize(e) {
+      console.log(terminalContainer.style.height)
+      terminalContainer.style.height  = window.innerHeight - e.pageY+ 'px';
+      setEditorHeight(e.pageY-90)
+      setTerminalHeight(window.innerHeight - e.pageY);
+      console.log(e.pageY)
+    }
+
+    function stopResize() {
+      window.removeEventListener('mousemove', resize)
+    }
+
+  }, []);
+
   return (
-    <main id="editor-page" className={`  bg-black overflow-hidden`}>
-      <nav className=" flex flex-wrap text-white col-start-1 bg-gray-900 border-b border-b-gray-600 col-end-3 items-center row-span-1">
+    <main id="editor-page" className={` grid-cols-[${sideBarWidth}px_1fr]  bg-black overflow-hidden`}>
+      <nav className=" flex flex-wrap text-white col-start-1 bg-gray-900 border-b border-b-gray-600 col-end-3 items-center row-start-1 row-end-2 ">
         {sideBarShown ? (
           <XIcon
             onClick={() => setSideBarShown(!sideBarShown)}
@@ -131,18 +180,22 @@ const EditorPage = () => {
         </button>
       </nav>
       <aside
+      id="sidebar"
+      style={{minHeight: 200}}
         className={`${
           sideBarShown ? "md:flex" : "hidden"
-        } hidden resize-x bg-gray-900/25`}
-      ></aside>
+        } hidden relative bg-gray-600 z-[9999] row-start-2 row-end-3`}
+      >
+        <div id="sidebar-resize-line" className=" absolute top-0 -right-1 h-full w-[8px] cursor-e-resize hover:border-r-[4px] hover:border-r-[#548ae8]"></div>
+      </aside>
       {/* <!-- Left Section: Resizable Horizontally --> */}
       <section
-        className={`grid grid-rows-[50px_1fr_200px] ${
+        className={`grid grid-rows-[50px_1fr_${terminalHeight}px] ${
           sideBarShown ? "md:col-start-2 " : "md:col-start-1"
-        } col-start-1 col-end-3 row-span-1`}
+        } col-start-1 col-end-3 row-start-2 row-end-3 `}
       >
         {/* <!-- Header Section --> */}
-        <header className="bg-black text-[#222831] flex flex-wrap items-center justify-between max-w-full px-4">
+        <header className="bg-black text-[#222831] row-start-1 row-end-2 flex flex-wrap items-center justify-between max-w-full px-4">
           <select
             onChange={handleSelect}
             name="lang"
@@ -169,7 +222,8 @@ const EditorPage = () => {
           language={language}
           defaultValue=""
           theme="vs-dark"
-          className=""
+          height={editorHeight}
+          className="row-start-2 row-end-3"
           value={editorContent.code}
           onChange={(value) =>
             setEditorContent({
@@ -178,10 +232,17 @@ const EditorPage = () => {
               lang: "python",
             })
           }
-          width={editorWidth}
+          width={editorWidth-1}
         />
 
-        <footer className=" text-[gray] bg-[#101010] grid grid-rows-[40px_1fr] border-t-[1.4px] border-t-gray-500">
+        <footer
+          id="terminal-container"
+          className="relative text-[gray] row-start-3 row-end-4 bg-gray-600/30 grid grid-rows-[40px_1fr] border-t-[1.4px] border-t-gray-500"
+        >
+          <div
+            id="resize-line"
+            className=" absolute -top-2 left-0 w-full h-[8px] hover:border-b-[3px] cursor-s-resize hover:border-b-[blue]"
+          ></div>
           <div className="flex gap-4 w-full px-4">
             {terminals.map((term, i) => (
               <button
