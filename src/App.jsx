7606da8f0b7b9903f155bdab2b508a550d6779c8
Keshav <maheshwari.keshav2090@gmail.com>
2024-06-28 23:01:28 +0530

    added backdrop feature

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Editor from "./pages/Editor"
 import Home from "./pages/Home"
 import Project from "./pages/Project"
 import {AboutPage} from './pages/About'
+import Room from "./Components/Room"
 
 const App = ()=> {
 
@@ -12,6 +13,7 @@ const App = ()=> {
       <Route path="/editor" element={<Editor/>}/>
       <Route path="/project" element={<Project/>}/>
       <Route path="/know-us" element={<AboutPage/>}/>
+      <Route path="/room" element={<Room/>}/>
     </Routes>
   )
 }
